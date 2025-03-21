4b0354bb38636705ab5b99390080ee2d252215d5
Emily Thomas <Thomas.ech@gmail.com>
2025-03-21 12:02:01 -0400

    basic art assets and starting player code

---
+++ player.gd
@@ -0,0 +1,45 @@
+extends Area2D
+
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+signal hit
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x > 0:
+		$AnimatedSprite2D.animation = "walk_right"
+	elif velocity.x < 0:
+		$AnimatedSprite2D.animation = "walk_left"
+	elif velocity.y > 0:
+		$AnimatedSprite2D.animation = "walk_up"
+	elif velocity.y < 0:
+		$AnimatedSprite2D.animation = "walk_down"
+
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
