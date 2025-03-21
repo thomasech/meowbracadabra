cb0416bf6478fa936a58af7aa2f5fc6528d32483
Emily Thomas <Thomas.ech@gmail.com>
2025-03-21 15:46:46 -0400

    genuinely no idea what I broke here but I broke something and fixed something else

--- main.gd
+++ main.gd
@@ -1,41 +1,35 @@
 extends Node
 
-
 @export var mob_scene: PackedScene
 var score
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	new_game()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func game_over() -> void:
+func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
-func new_game() -> void:
+
+func new_game():
+	get_tree().call_group(&"mobs", &"queue_free")
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
-func _on_start_timer_timeout() -> void:
-	$MobTimer.start()
-	$ScoreTimer.start()
-
-func _on_mob_timer_timeout() -> void:
+func _on_MobTimer_timeout():
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
 
 	# Choose a random location on Path2D.
-	var mob_spawn_location = $MobPath/MobSpawnLocation
-	mob_spawn_location.progress_ratio = randf()
+	var mob_spawn_location = get_node(^"MobPath/MobSpawnLocation")
+	mob_spawn_location.progress = randi()
 
-	# Set the mob's position to the random location.
+	# Set the mob's position to a random location.
 	mob.position = mob_spawn_location.position
 
 	# Set the mob's direction perpendicular to the path direction.
@@ -52,5 +46,11 @@ func _on_mob_timer_timeout() -> void:
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
 
-func _on_score_timer_timeout() -> void:
+func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
